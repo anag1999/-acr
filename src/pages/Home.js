02c9391fa81fa25bc13e0bac585d5bedfe20bf9a
Irving Juarez <irvingjuarez274@gmail.com>
2021-11-06 10:28:29 -0600

    The first part of the home page is done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,26 @@
 const Home = () => {
     const view = `
-        <h2>Home</h2>
+        <section class="main-introduction">
+            <article class="introduction-cover">
+                <div class="introduction-cover_text">
+                    Expertos en el suministro de material para tu empresa.
+                </div>
+            </article>
+
+            <article class="introduction-description">
+                <p>
+                    En Comercializadora ACR encontraras todos los materiales que tu empresa necesita: papeleria, salud, seguridad y mucho más a un costo accesible.
+                </p>
+
+                <h2>¿Qué producto necesitas?</h2>
+            </article>
+
+            <article class="introduction-button">
+                <button>
+                    ¡AQUÍ TE LO COTIZAMOS!
+                </button>
+            </article>
+        </section>
     `;
 
     return view;
