404b5c4080e116104dfa13c45b6072231e9c0d28
Irving Juarez <irvingjuarez274@gmail.com>
2021-11-06 12:07:47 -0600

    About section in responsive done

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,65 @@
 const About = () => {
     const view = `
-        <h2>About</h2>
+        <section class="main-about">
+            <span></span>
+            <h2>¿Quiénes somos?</h2>
+
+            <article class="about_us">
+                <div>
+                    <h2>Nosotros</h2>
+                </div>
+
+                <p>
+                    Somos una empresa dedicada a la comercialización de productos nacionales e internacionales a un
+                    costo accesible
+                </p>
+            </article>
+
+            <article class="about_mision">
+                <div>
+                    <h2>Misión</h2>
+                </div>
+
+                <p>
+                Satisfacer las necesidades de nuestros clientes, brindando servicio de calidad y al mejor precio
+                del mercado
+                </p>
+            </article>
+
+            <article class="about_vision">
+                <div>
+                    <h2>Visión</h2>
+                </div>
+
+                <p>
+                Ser el proveedor No.1 en la Ciudad de México
+                y constituirnos como la casa comercializadora líder
+                a nivel nacional, siempre comprometidos en cubrir las necesidades de nuestros clientes
+                </p>
+            </article>
+
+            <article class="about_values">
+                <div>
+                    <h2>Valores</h2>
+                </div>
+
+                <p>
+                En la Comercializadora ACR tenemos como compromiso ofrecer productos de primer nivel
+                a un precio accesible, ello siempre basado
+                en los siguientes valores de colaboración
+                con nuestros clientes:
+                </p>
+            </article>
+
+            <ul>
+                <li>Compromiso</li>
+                <li>Responsabilidad</li>
+                <li>Disciplina</li>
+                <li>Honestidad</li>
+                <li>Puntualidad</li>
+                <li>Excelencia</li>
+            </ul>
+        </section>
     `;
 
     return view;
