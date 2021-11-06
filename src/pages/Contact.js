4071f5535747ea40389f777cf0130f116f37e6b7
Irving Juarez <irvingjuarez274@gmail.com>
2021-11-06 14:28:45 -0600

    The contact page responsive version is already done

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,6 +1,51 @@
 const Contact = () => {
     const view = `
-        <h2>Contact</h2>
+        <section class="main-contact">
+            <span></span>
+            <h2>Contacto</h2>
+
+            <p>
+                ¿Tienes alguna duda?
+                <br>
+                ¡Contáctanos!
+            </p>
+
+            <p>
+            Será un gusto ayudarte a encontrar 
+            los productos para tu empresa
+            </p>
+
+            <form>
+                <div>
+                    <label for="name">Nombre</label>
+                    <input type="text" id="name">
+                </div>
+
+                <div>
+                    <label for="phone">Celular</label>
+                    <input type="tel" id="phone">
+                </div>
+
+                <div>
+                    <label for="email">Correo electronico</label>
+                    <input type="email" id="email">
+                </div>
+
+                <div>
+                    <label for="comment">¿Duda, sugerencia o comentario?:</label>
+                    <textarea id="comment" placeholder="Escribe un comentario"></textarea>
+                </div>
+
+                <button>Enviar</button>
+            </form>
+
+            <article class="main-contant_media">
+                <img src="./assets/imgs/facebook.png" />
+                <img src="./assets/imgs/email.png" />
+                <img src="./assets/imgs/whatsapp.png" />
+                <img src="./assets/imgs/phone-call.png" />
+            </article>
+        </section>
     `;
 
     return view;
