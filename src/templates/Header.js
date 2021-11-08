b9870df15377cdd28f291087d911fa24c3c87d62
Irving Juarez <irvingjuarez274@gmail.com>
2021-11-08 13:36:56 -0600

    Bug fixed

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
                 <img src="./assets/icons/x.svg" />
             </button>
             <ul>
-                <li><a href="/">Inicio</a></li>
+                <li><a href="/acr/">Inicio</a></li>
                 <li><a href="#/about">¿Quiénes somos?</a></li>
                 <li><a href="#/products">Productos</a></li>
                 <li><a href="#/contact">Contacto</a></li>
